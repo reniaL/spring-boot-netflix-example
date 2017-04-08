984eea4e2032dcab971f2197419e1a0eb087491a
hrchen <hrchen@corp.netease.com>
2017-04-08 21:57:52 +0800

    rename package

--- eureka-demo-notification/src/main/java/com/github/renial/eureka/notification/NotificationMicroService.java
+++ eureka-demo-notification/src/main/java/com/github/renial/eureka/notification/NotificationMicroService.java
@@ -1,4 +1,4 @@
-package com.deswaef.netflixexamples.notifications;
+package com.github.renial.eureka.notification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- eureka-demo-server/src/main/java/com/github/renial/eureka/server/EurekaServer.java
+++ eureka-demo-server/src/main/java/com/github/renial/eureka/server/EurekaServer.java
@@ -1,4 +1,4 @@
-package com.deswaef.netflixexamples.eureka;
+package com.github.renial.eureka.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
