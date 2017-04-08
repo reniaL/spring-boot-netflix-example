334a130ade7dc3e5c4e90a8e6a84ca0b04c990ed
hrchen <hrchen@corp.netease.com>
2017-04-08 21:19:52 +0800

    update dependency version

--- notifications-service/src/main/java/com/deswaef/netflixexamples/notifications/NotificationMicroService.java
+++ notifications-service/src/main/java/com/deswaef/netflixexamples/notifications/NotificationMicroService.java
@@ -1,11 +1,13 @@
 package com.deswaef.netflixexamples.notifications;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.system.ApplicationPidFileWriter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.system.ApplicationPidFileWriter;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 @EnableEurekaClient //or @EnableDiscoveryClient
 public class NotificationMicroService {
 
