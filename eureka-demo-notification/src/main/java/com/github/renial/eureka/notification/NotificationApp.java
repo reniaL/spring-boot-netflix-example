b606ccbc70caf69d07c54f4f4c56b7b3d00fcdc3
hrchen <hrchen@corp.netease.com>
2017-04-13 01:09:59 +0800

    simplify by removing hystrix

--- eureka-demo-notification/src/main/java/com/github/renial/eureka/notification/NotificationApp.java
+++ eureka-demo-notification/src/main/java/com/github/renial/eureka/notification/NotificationApp.java
@@ -3,17 +3,11 @@ package com.github.renial.eureka.notification;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.system.ApplicationPidFileWriter;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableAsync
 @EnableEurekaClient
-@EnableCircuitBreaker
-@EnableHystrixDashboard
 @EnableFeignClients
 public class NotificationApp {
 
